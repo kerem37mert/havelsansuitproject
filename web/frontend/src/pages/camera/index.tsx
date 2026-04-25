afcbb3364faa983a15998900b67c8883f3250af9
kerem37mert <kerem37mert@yahoo.com>
2026-04-25 14:55:56 +0300

    frontend updated

--- web/frontend/src/pages/camera/index.tsx
+++ web/frontend/src/pages/camera/index.tsx
@@ -1,8 +1,48 @@
+import { useEffect, useRef } from "react";
+
+const WHEP_URL = "https://havelsansuit.dev/whip/kamera";
+
 const Camera = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const pc = new RTCPeerConnection({
+      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+    });
+
+    pc.ontrack = (e) => {
+      if (videoRef.current) {
+        videoRef.current.srcObject = e.streams[0];
+      }
+    };
+
+    pc.addTransceiver("video", { direction: "recvonly" });
+
+    pc.createOffer()
+      .then((offer) => pc.setLocalDescription(offer))
+      .then(() =>
+        fetch(WHEP_URL, {
+          method: "POST",
+          headers: { "Content-Type": "application/sdp" },
+          body: pc.localDescription.sdp,
+        })
+      )
+      .then((res) => res.text())
+      .then((sdp) =>
+        pc.setRemoteDescription({ type: "answer", sdp })
+      );
+
+    return () => pc.close();
+  }, []);
+
   return (
-    <div>
-      only video
-    </div>
+    <video
+      ref={videoRef}
+      autoPlay
+      muted
+      playsInline
+      style={{ width: "100%", height: "100%" }}
+    />
   );
 };
 
